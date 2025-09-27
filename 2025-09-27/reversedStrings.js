d4fb47a03ba23cca12c4d710954b613d446f3cb3
Kangoor Brown <kangoorbrown@wp.pl>
2025-09-27 11:13:22 +0200

    2025-09-27: solved 'Reversed Strings'

---
+++ 2025-09-27/reversedStrings.js
@@ -0,0 +1,4 @@
+function solution(str){
+  // https://www.freecodecamp.org/news/how-to-reverse-a-string-in-javascript-in-3-different-ways-75e4763c68cb/
+  return str.split("").reverse().join("");
+}
