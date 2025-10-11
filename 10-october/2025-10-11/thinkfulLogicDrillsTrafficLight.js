492439452ea9684abe3231a189a66f0736058aff
Kangoor Brown <kangoorbrown@wp.pl>
2025-10-11 23:17:21 +0200

    2025-10-11: solved 'Thinkful Logic Drills Traffic Light' kata

---
+++ 10-october/2025-10-11/thinkfulLogicDrillsTrafficLight.js
@@ -0,0 +1,12 @@
+const updateLight = (current) => {
+  switch (current) {
+    case 'green':
+      return 'yellow';
+    case 'yellow':
+      return 'red';
+    case 'red':
+      return 'green';
+    default:
+      return 'unknown light';
+  }
+}
