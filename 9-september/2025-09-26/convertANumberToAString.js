e88362468d184428cb4a30e938480f8bfaf97c35
Kangoor Brown <kangoorbrown@wp.pl>
2025-10-15 18:34:20 +0200

    2025-10-15: comment 'Convert a Number to a String' kata

--- 9-september/2025-09-26/convertANumberToAString.js
+++ 9-september/2025-09-26/convertANumberToAString.js
@@ -1,3 +1,23 @@
+/*
+  // 🦘 Kangaroo’s Number Game //
+
+  Turning numbers into strings, huh?  
+  Back in Poznań I was turning cheap vodka into bad decisions,  
+  and coins into cigarettes. Different kind of conversion, same technique —  
+  desperation and a bit of math.  
+
+  My mate told me there’s money in coding,  
+  so I stopped counting bottles and started counting characters.  
+  Never checked justjoinit, didn’t care about the pay —  
+  I just knew that every bug I fix is a shot closer  
+  to the Melbourne nights I left behind.  
+
+  It’s funny, you know —  
+  all I’m doing now is wrapping cold numbers in quotes  
+  so they look a bit more human.  
+  Guess I’ve been doing that to myself for years.
+*/
+
 function numberToString(num) {
   // https://stackoverflow.com/questions/5765398/whats-the-best-way-to-convert-a-number-to-a-string-in-javascript
   return '' + num;
