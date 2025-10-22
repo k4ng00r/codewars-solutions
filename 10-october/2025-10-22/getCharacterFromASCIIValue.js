bfe6cfb76f9c4ff87812ca4b31594a8630432658
Kangoor Brown <kangoorbrown@wp.pl>
2025-10-22 17:46:34 +0200

    2025-10-22: solved 'get character from ASCII Value' kata

---
+++ 10-october/2025-10-22/getCharacterFromASCIIValue.js
@@ -0,0 +1,8 @@
+// Aussie chaos begins
+const getChar = c => String.fromCharCode(c)
+// so I write this tiny line and suddenly I feel like a hacker again ( ͡° ͜ʖ ͡°)
+// when I was a kid in Melbourne I thought ASCII was a sushi roll
+// now I’m in Poznań and it’s just the alphabet telling me to stop drinking
+// 65 -> 'A' ... 97 -> 'a' ... 48 -> '0' ... easy, clean, like my therapist wishes my liver was
+// one line of code, one step closer to that ticket back to Sydney
+// (╯°□°）╯︵ ┻━┻ why does this even work so perfectly, who wrote this language drunker than me?
