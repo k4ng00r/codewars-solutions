3e957ce75844bd7cd2cb4d2e7683075c2cb05721
Kangoor Brown <kangoorbrown@wp.pl>
2025-10-22 17:58:51 +0200

    2025-10-22: solved 'Basic variable assignment' kata

---
+++ 10-october/2025-10-22/basicVariableAssignent.js
@@ -0,0 +1,9 @@
+// Aussie chaos begins
+var a = "code";
+var b = "wa.rs";
+var name = a + b;
+// mate, someone had written '==' instead of '=', like they were trying to compare their life to mine
+// it’s assignment, not a Tinder match ( ͡° ͜ʖ ͡°)
+// anyway, now it works, name = "codewa.rs"
+// pure poetry, unlike my rent contract in Poznań
+// I fixed it and still feel empty inside, but at least JavaScript’s happy
