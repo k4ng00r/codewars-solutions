2dcf1084ae10b91b6eec34feffb2bb9e7d3fbb2c
Kangoor Brown <kangoorbrown@wp.pl>
2025-10-22 18:20:38 +0200

    2025-10-22: solved 'Exclusive \'or\' (xor) Logical Operator' kata

---
+++ 10-october/2025-10-22/exclusiveOrXorLogicalOperator.js
@@ -0,0 +1,19 @@
+///////////////////////////////
+// Aussie chaos begins here  //
+// Kangur woke up, half-dead //
+// brain like mashed potatoes //
+///////////////////////////////
+
+const xor = (a, b) => 
+    // so basically we’re saying:
+    // “if one’s drunk and the other’s sober, we call that balance”
+    (a || b) && !(a && b);
+
+/////////////////////////////////////////////
+// Jesus wept, it actually works ( ͡° ͜ʖ ͡°)
+// one true, one false – story of my exes
+// both true? forget it. both false? just like my bank account.
+// logical perfection born in chaos.
+// maybe I should XOR my emotions next time.
+// anyway, cheers, JavaScript – you beautiful bastard.
+/////////////////////////////////////////////
