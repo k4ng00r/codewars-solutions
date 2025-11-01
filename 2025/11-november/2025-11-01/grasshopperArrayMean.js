3c4d9260408d618de368200ce9f9892a546d3969
Kangoor Brown <kangoorbrown@wp.pl>
2025-11-01 20:25:44 +0100

    2025-11-01: solved'Grasshopper - Array Mean' kata

---
+++ 2025/11-november/2025-11-01/grasshopperArrayMean.js
@@ -0,0 +1,5 @@
+// Night audit on a tram: empty out the pocket full of numbers, pile them together,
+// then split the stash evenly by how many hands were in the game. Fair share, calmer heart. ( ͡° ͜ʖ ͡°)
+// Mini-timeline: gather → sum → divide → breathe; Melbourne morning feels closer already.
+const findAverage = nums => nums.reduce((curr, acc) => curr + acc) / nums.length;
+// Footnote from the bar napkin: if the list was a long night, this is the quiet, honest middle. ლ(ಠ益ಠლ)
