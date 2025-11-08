de212b941ff9f7d5bbf77dcf0ed7bc4690a2071d
Kangoor Brown <kangoorbrown@wp.pl>
2025-11-08 11:38:47 +0100

    2025-11-08: solved 'Kata Example Twist' kata

---
+++ 2025/11-november/2025-11-08/kataExampleTwist.js
@@ -0,0 +1,7 @@
+// thousand tiny victories marching in formation ( ͡° ͜ʖ ͡°)
+// spin up an array of length 1000, fill every seat with "codewars", close the door
+// no loops, no drama, just a neat factory line while the city hisses outside
+// Poznań rain on the pane, Melbourne sun in my head; it still works, and I’m weirdly proud
+// if this breaks, I’ll eat my keyboard case. k***a.
+
+let websites = new Array(1000).fill('codewars');
