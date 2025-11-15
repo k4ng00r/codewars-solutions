cdc29bb855ea746e5d9ecb3e7eda89a1104cab8b
Kangoor Brown <kangoorbrown@wp.pl>
2025-11-15 18:32:55 +0100

    2025-11-15: solved 'Chuck Norris VII - True or False? (Beginner)' kata

---
+++ 2025/11-november/2025-11-15/chuckNorrisVIITrueOrFalseBeginner.js
@@ -0,0 +1,9 @@
+// trying to out-Chuck Chuck, bold move mate ( ͡° ͜ʖ ͡°)
+// rule 1: when Chuck says "no", the universe refactors itself
+function ifChuckSaysSo(){
+  // truth walks in all confident, then his biceps show up and logic just taps out
+  // !true = "reality got stared down and backed off like a scared kangaroo on a freeway"
+  // flashback: last time I trusted plain true, woke up in Poland writing katas instead of surfing in Melbourne
+  return !true;
+  // if this ever returns true, it means Chuck reviewed your code personally and you should be very, very concerned (╯°□°）╯︵ ┻━┻
+}
