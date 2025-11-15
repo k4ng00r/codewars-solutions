ca2e1c21c46168348f231b8cfddadaed2d9ca559
Kangoor Brown <kangoorbrown@wp.pl>
2025-11-15 19:18:03 +0100

    2025-11-15: solved 'BASIC: Making Six Toast' kata

---
+++ 2025/11-november/2025-11-15/basicMakingSixToast.js
@@ -0,0 +1,11 @@
+// toast speedrun: because waiting for breakfast is for responsible adults ( ͡° ͜ʖ ͡°)
+// goal: always end up exactly at 6 pieces, no matter how overcaffeinated you were
+function sixToast(num) {
+  // num = how many slices you *think* you tossed in
+  // 6 = the sacred number, like beers on a Friday "just one more" night
+  // ternary time: if you overshot (num >= 6), take some out
+  // if you undershot, cram a few more in before they get cold
+  return num >= 6 ? num - 6 : 6 - num;
+  // result is always positive: code doesn't do drama, it just tells you how wrong you were
+  // flashback: burned toast in a tiny Poznań kitchen, late JS tutorial playing, me pretending this is "the grind"
+}
