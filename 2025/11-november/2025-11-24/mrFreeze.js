e2e5f37a5dede7d1421ebf1db01e6e4475c52670
Kangoor Brown <kangoorbrown@wp.pl>
2025-11-24 15:30:08 +0100

    2025-11-24: solved 'Mr. Freeze' kata

---
+++ 2025/11-november/2025-11-24/mrFreeze.js
@@ -0,0 +1,5 @@
+// MrFreeze: not a rapper, just an object sentenced to eternal immutability ( ͡° ͜ʖ ͡°)
+// once frozen, no new props, no changes, no deletes – pure JavaScript Siberia
+Object.freeze(MrFreeze);
+// somewhere in memory he’s screaming “let me change just ONE thing”
+// JS: “nah mate, spec says you stay like this forever”
